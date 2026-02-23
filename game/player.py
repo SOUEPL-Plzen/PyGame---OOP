acb5a00158266f5419b41bf3dd2301ca2d9d2ee0
panucitelcz <petr.b257@seznam.cz>
2026-02-23 13:38:45 +0100

    Pata hodina

--- game/player.py
+++ game/player.py
@@ -46,3 +46,36 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_SPACE] and self.on_ground:
             self.velocity_y = -JUMP_POWER
             self.on_ground = False
+
+        self.velocity_y += GRAVITY
+        if self.velocity_y > 15:
+            self.velocity_y = 15
+        
+        self.rect.x += self.velocity_x
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        
+        self.rect.y += self.velocity_y
+
+        self.on_ground = False
+
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.velocity_y > 0:
+                    self.rect.bottom = platform.rect.top
+                    self.velocity_y = 0
+                    self.on_ground = True
+                elif self.velocity_y < 0:
+                    self.rect.top = platform.rect.bottom
+                    self.velocity_y = 0 
+
+        if self.rect.top > SCREEN_HEIGHT:
+            return True
+        return False
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)       
+
+pygame.sprite.Sprite.update()
